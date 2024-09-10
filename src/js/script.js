acaa7ac4e99d3528165b415ad9d8ffa84727144d
Neto6391 <neto6391@gmail.com>
2024-09-10 01:36:30 -0300

    feat: create special calculations

--- src/js/script.js
+++ src/js/script.js
@@ -32,6 +32,54 @@ function appendToDisplay(value) {
     }
 }
 
+function percentage() {
+    if (currentOperator === '' && firstNumber) {
+        display.value = parseFloat(firstNumber) / 100;
+        firstNumber = display.value;
+    } else if (currentOperator !== '' && secondNumber) {
+        display.value = (parseFloat(firstNumber) * parseFloat(secondNumber)) / 100;
+        secondNumber = display.value;
+    }
+}
+
+function reciprocal() {
+    if (display.value !== '0') {
+        display.value = 1 / parseFloat(display.value);
+        if (currentOperator === '') {
+            firstNumber = display.value;
+        } else {
+            secondNumber = display.value;
+        }
+    }
+}
+
+function square() {
+    display.value = Math.pow(parseFloat(display.value), 2);
+    if (currentOperator === '') {
+        firstNumber = display.value;
+    } else {
+        secondNumber = display.value;
+    }
+}
+
+function squareRoot() {
+    display.value = Math.sqrt(parseFloat(display.value));
+    if (currentOperator === '') {
+        firstNumber = display.value;
+    } else {
+        secondNumber = display.value;
+    }
+}
+
+function toggleSign() {
+    display.value = parseFloat(display.value) * -1;
+    if (currentOperator === '') {
+        firstNumber = display.value;
+    } else {
+        secondNumber = display.value;
+    }
+}
+
 function clearDisplay() {
     display.value = '0';
     history.textContent = '';
